d9fddb5b990e603bd7c46d55bd69e317824466b7
ethanjfobrien <ethanjfobrien@gmail.com>
2024-06-01 12:45:18 +0000

    test CI/CD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hello ${name}! we are live on CI/CID with cloud run!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
